05618a3a3d70b65c83793243d87b5e9757dbfe78
Mavroian and Konstantin <mavflorin+konst2011@gmail.com>
2019-05-01 16:25:57 +0900

    Implemented AllPhotots and single Photo into app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import "../styles/styles.css";
 
+import Navbar from "./Navbar";
+import SinglePhoto from "./SinglePhoto";
+import Upload from "./Upload";
+import AllPhotos from "./AllPhotos";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentView: "",
+      photos: [],
+      selectedPhoto: ""
+    };
   }
 
   render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
+        <Navbar />
+        {this.state.currentView === "SinglePhoto" && <SinglePhoto />}
+        {this.state.currentView === "AllPhotos" && <AllPhotos />}
       </div>
-    )
+    );
   }
 }
--- src/components/SinglePhoto.jsx
+++ src/components/SinglePhoto.jsx
@@ -1,1 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
+
+export default class SinglePhoto extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
