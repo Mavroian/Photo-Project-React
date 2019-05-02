dd6fc00b36551a26f8297c265ce599250a5fec92
Mavroian and Konstantin <mavflorin+konst2011@gmail.com>
2019-05-02 17:33:21 +0900

    Uploads Complete

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -5,9 +5,39 @@ import "../styles/upload.css";
 export default class Upload extends Component {
   constructor(props) {
     super(props);
+    this.file = null;
+
+    this.setFileInputRef = element => {
+      this.file = element;
+    };
+
+    this.clickFileInput = () => {
+      // Focus the text input using the raw DOM API
+      if (this.file) this.file.click();
+    };
   }
+  componentDidMount() {
+    // autofocus the input on mount
+    this.clickFileInput();
+  }
+  onFormSubmit = event => {
+    this.props.uploadPhoto(event.target.files[0]);
+  };
 
   render() {
-    return <div className="file-upload" />;
+    return (
+      <div className="file-upload">
+        <form onInputCapture={this.onFormSubmit} action="">
+          <input type="file" ref={this.setFileInputRef} />
+          <input
+            name="form"
+            className="button"
+            type="button"
+            value="click the text input"
+            onClick={this.clickFileInput}
+          />
+        </form>
+      </div>
+    );
   }
 }
