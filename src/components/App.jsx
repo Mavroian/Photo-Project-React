fe1da6517e0da49e0586860df56f0c7ef63414ba
Mavroian and Konstantin <mavflorin+konst2011@gmail.com>
2019-05-02 17:32:21 +0900

    App implementation complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,30 +6,75 @@ import SinglePhoto from "./SinglePhoto";
 import Upload from "./Upload";
 import AllPhotos from "./AllPhotos";
 
-import { getSingleObject } from "../utils";
-
-console.log(getSingleObject("0.png").then(result => result));
+import { getSingleObject, listObjects } from "../utils";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.arr = [];
     this.state = {
-      currentView: "AllPhotos",
+      currentView: "",
       photos: [],
       selectedPhoto: ""
     };
   }
-  setSinglePhoto() {
-    this.setState({ currentView: "SinglePhoto" });
+
+  componentDidMount() {
+    listObjects()
+      .then(result => {
+        result.forEach(element => {
+          getSingleObject(element.Key).then(obj => {
+            this.arr.push(obj);
+            this.setState({ photos: this.arr });
+          });
+        });
+      })
+      .then(end => {
+        this.setState({ currentView: "AllPhotos" });
+      });
   }
+
+  setSinglePhoto = base64 => {
+    this.setState({ currentView: "SinglePhoto", selectedPhoto: base64 });
+  };
+
+  setAllPhoto = () => {
+    this.setState({ currentView: "AllPhotos", selectedPhoto: "" });
+  };
+
+  setUpload = () => {
+    this.setState({ currentView: "Upload", selectedPhoto: "" });
+  };
+
+  uploadPhoto = file => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.arr.push(
+        reader.result.slice(
+          reader.result.indexOf(",") + 1,
+          reader.result.length - 1
+        )
+      );
+      this.setState({ photos: this.arr });
+    };
+  };
 
   render() {
     return (
       <div className="app">
-        <Navbar />
-        {this.state.currentView === "SinglePhoto" && <SinglePhoto />}
+        <Navbar currentView={this.setAllPhoto} setUpload={this.setUpload} />
+        {this.state.currentView === "SinglePhoto" && (
+          <SinglePhoto selectedPhoto={this.state.selectedPhoto} />
+        )}
+        {this.state.currentView === "Upload" && (
+          <Upload uploadPhoto={this.uploadPhoto} />
+        )}
         {this.state.currentView === "AllPhotos" && (
-          <AllPhotos currentView={this.setSinglePhoto} />
+          <AllPhotos
+            currentView={this.setSinglePhoto}
+            photos={this.state.photos}
+          />
         )}
       </div>
     );
